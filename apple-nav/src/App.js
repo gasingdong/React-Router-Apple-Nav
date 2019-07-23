8079ae16065ffafe557497b1763952dae2d77c75
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-23 12:44:28 -0700

    add basic NavWrapper component

--- apple-nav/src/App.js
+++ apple-nav/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavWrapper from './components/NavWrapper';
 
 function App() {
   return (
     <div className="App">
       <header className="nav-header">
+        <NavWrapper />
       </header>
     </div>
   );
---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function NavWrapper() {
+    return (
+        <nav>
+            <ul>
+                <li>Mac</li>
+                <li>iPad</li>
+                <li>iPhone</li>
+                <li>Watch</li>
+                <li>TV</li>
+                <li>Music</li>
+                <li>Support</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavWrapper;
